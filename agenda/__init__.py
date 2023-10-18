e24480b6f6d0e22f011faae252d3e84d4986d4f9
lupitallamas <lupitallt64@gmail.com>
2023-10-17 20:37:22 -0600

    parte uno del proyecto

---
+++ agenda/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from .api import api
+from .config import Confing
+from .db import db
+
+def create_app():
+    
+    app = Flask(__name__)
+    
+    app.config.from_object(Confing)
+    
+    db.init_app(app)
+    app.register_blueprint(api)
+    
+    @app.route("/init_db")
+    def init_db():
+        db.create_all()
+        return "DataBase Create"
+    
+    @app.route("/drop_db")
+    def drop_db():
+        db.drop_all()
+        return "Database Droppedlo Koders!"
+    
+    @app.route("/")
+    def hello():
+        return "Hello amiguitos"
+    
+    return app
---
+++ agenda/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Confing:
+    
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    
+    
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+    )
